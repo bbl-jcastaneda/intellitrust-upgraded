b535a24b7b861ac2199beda39f0647a6fb14db25
Jahdiel C <jcastaneda@belizebank.com>
2023-10-05 16:54:01 -0600

    added type definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,8 @@
 export interface IntellitrustPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+  createNewSoftTokenIdentity(options:{args:any}): Promise<{ value: string }>;
+  getOTP(options:{args:any}): Promise<{ value: string }>;
+  parseNotification(options:{args:any}): Promise<{ value: string }>;
+  completeTransaction(options:{args:any}): Promise<{ value: string }>;
+  deleteIdentity(options:{args?: any}): any;
 }
--- src/web.ts
+++ src/web.ts
@@ -3,6 +3,26 @@ import { WebPlugin } from '@capacitor/core';
 import type { IntellitrustPlugin } from './definitions';
 
 export class IntellitrustWeb extends WebPlugin implements IntellitrustPlugin {
+  deleteIdentity(options: { args?: any; }) {
+    console.log('deleteIdentity', options);
+    return options;
+  }
+  createNewSoftTokenIdentity(options: { args: any; }): any {
+    console.log('createNewSoftTokenIdentity', options);
+    return options;
+  }
+  getOTP(options: { args: any; }): any {
+    console.log('getOTP', options);
+    return options;
+  }
+  parseNotification(options: { args: any; }): any {
+    console.log('parseNotification', options);
+    return options;
+  }
+  completeTransaction(options: { args: any; }): any {
+    console.log('completeTransaction', options);
+    return options;
+  }
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
