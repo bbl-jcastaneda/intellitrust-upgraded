5c28f382a2df492c4fd855afdde169c4634bdf8e
Jahdiel C <jcastaneda@belizebank.com>
2023-10-05 16:26:13 -0600

    rewrote main file to swift

---
+++ ios/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  Plugin
+//
+//  Created by Isd Manager on 05/10/23.
+//  Copyright © 2023 Max Lynch. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+
+#endif /* BridgingHeader_h */
